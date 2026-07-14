63d8755a72c26c6d1fa6759135487b9a848a065a
Claudio Maza <143014265+claudiomaza@users.noreply.github.com>
2026-07-13 21:32:06 -0300

    cm2labs: remove unsplash from image config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: '*.supabase.co' },
     ],
   },
